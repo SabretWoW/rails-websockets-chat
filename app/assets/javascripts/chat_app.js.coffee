6f45ef643e3af545fe96d7aef123e96786a465df
Kyle Carlson <thekylecarlson@gmail.com>
2013-09-08 15:11:57 -0500

    user count in sidebar updates as users connect/disconnect

--- app/assets/javascripts/chat_app.js.coffee
+++ app/assets/javascripts/chat_app.js.coffee
@@ -1,24 +1,22 @@
 $(document).ready ->
 	
-	ws = new WebSocketRails('localhost:3000/websocket')
+	dispatcher = new WebSocketRails('localhost:3000/websocket')
 
-	ws.on_open = ->
+	dispatcher.on_open = ->
 		console.log 'socket opened'
 
-	ws.bind 'new_message', (data) =>
-		console.log data
+	dispatcher.bind 'new_message', (data) =>
 		$('#chat_log')
 			.append("<div>" + data['user'] + ": " + data['text'] + "</div>").fadeIn()
 		$("#chat_log").animate
-      scrollTop:$("#chat_log")[0].scrollHeight - $("#chat_log").height()
-		
+			scrollTop:$("#chat_log")[0].scrollHeight - $("#chat_log").height()
+		console.log("#{data['user_count']}")
+		$('#user_count').text(data['user_count'])
 
 	$('#send_message').on 'click', =>
-		# storing the message box text in a variable since we'll probably use it later 
 		message = $('#new_message').val()
-		ws.trigger 'send_message', {text: message} if message.length > 0
+		dispatcher.trigger 'send_message', {text: message} if message.length > 0
 		$('#new_message').val('')
 
 	$('#new_message').keypress (e) -> 
-		$('#send_message').click() if (e.keyCode == 13 && $('#new_message').val().length > 0)	
-
+		$('#send_message').click() if (e.keyCode == 13 && $('#new_message').val().length > 0)
--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,25 +1,40 @@
 class ChatController < WebsocketRails::BaseController
 
 	def client_connected
-    connection_store[:screen_name] = "Chatter #{Random.rand(1000)}"
-    broadcast_message :new_message, {:user => "System", :text => "New user #{connection_store[:screen_name]} connected!" }
+    connection_store[:username] = "Chatter #{Random.rand(1000)}"
+    connection_store[:fav_number] = Random.rand(20)
 
+    collected_users = connection_store.collect_all(:username)
+    user_count = collected_users.count
+
+2.times { puts "client_connected"
+          puts "================"
+          puts "collected_users:  #{collected_users}"
+          puts "user_count:       #{user_count}"
+          puts "\n" 
+        }
+
+    broadcast_message :new_message, {:user => "System", :text => "New user #{connection_store[:username]} connected!  (Users: #{user_count})", user_count: user_count }
+    # broadcast_message :update_
   end
 
 	def send_message
-		screen_name = connection_store[:screen_name]
-users = connection_store.collect_all(:screen_names)
-10.times { puts users }
-		broadcast_message :new_message, {:user => screen_name, :text => message[:text] } #, :user_count => users}
+		username = connection_store[:username]
+users = connection_store.collect_all(:username)
 
+1.times { message.merge!(username: connection_store[:username]) }
+1.times { message.merge!(user_count: users.count) }
+10.times { puts message.inspect + "\n" }
+		broadcast_message :new_message, {:user => username, :text => message[:text], :user_count => message[:user_count] }
 
 	end
 
 	def set_name
-		connection_store[:screen_name] = message[:name]
+		connection_store[:username] = message[:name]
 	end
 
   def client_disconnected
-    broadcast_message :new_message, {:user => "System", :text => "#{connection_store[:screen_name]} disconnected." }
+    user_count = connection_store.collect_all(:connection_id).count
+    broadcast_message :new_message, {:user => "System", :text => "#{connection_store[:username]} disconnected. (Users: #{user_count})", user_count: user_count }
   end
 end
